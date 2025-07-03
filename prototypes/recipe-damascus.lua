728c3b9aaf3b34667925cf9f5b4e45d28cb46ad5
Elixir-Exoprosopa <raviravirego@gmail.com>
2025-07-03 18:21:35 -0300

    Update recipe-damascus.lua

--- prototypes/recipe-damascus.lua
+++ prototypes/recipe-damascus.lua
@@ -6,7 +6,7 @@ data:extend({
     energy_required = 3.2,
     emissions_multiplier = 6,
     auto_recycle = false,
-    ingredients = { { type = "item", name = "damascusore", amount = 1 } },
+    ingredients = { { type = "fluid", name = "water", amount = 1 } },
     results = { { type = "item", name = "damascus", amount = 1 } },
     allow_decomposition = false,
     allow_productivity = true,
