1754b01e3b00b6a06f4900df774899ce48b01736
Elixir-Exoprosopa <raviravirego@gmail.com>
2025-07-03 18:07:12 -0300

    Update damascus-ore.lua

--- prototypes/damascus-ore.lua
+++ prototypes/damascus-ore.lua
@@ -4,7 +4,7 @@ xenroserio.damascusore = {
   tint = { r = 0.416, g = 0.38, b = 0.322 },
   map_color = { r = 0.416, g = 0.38, b = 0.322 },
   mining_time = 0.8,
-  enabled = false,
+  enabled = true,
   icon = "__xenroserio__/graphics/icons/damascus-ore.png",
   stage_mult = 100,
   item = {
