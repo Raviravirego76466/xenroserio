abff7717c8ddb69e4acb763c6cd79855593da1df
Elixir-Exoprosopa <raviravirego@gmail.com>
2025-07-03 17:42:05 -0300

    Update damascus-item.lua

--- prototypes/damascus-item.lua
+++ prototypes/damascus-item.lua
@@ -13,7 +13,7 @@ data:extend({
     icon = "__xenroserio__/graphics/icons/damascus.png",
     icon_size = 32,
     subgroup = "intermediate-products",
-    order = "c",
+    order = "a",
     stack_size = 200,
     drop_sound = plate_drop_move,
     inventory_move_sound = plate_drop_move,
